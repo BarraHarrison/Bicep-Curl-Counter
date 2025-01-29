52d216ca55ec1e075adb9da20b6703ebcd24c260
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-29 16:46:46 +0900

    Model class created and the Image and Class lists begin as empty numpy arrays

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ import PIL
 import numpy as np 
 from sklearn.svm import LinearSVC
 
+class Model():
+    
+    def __init__(self):
+        self.model = LinearSVC()
+
+    def train_model(self, counters):
+        img_list = np.array([])
+        class_list = np.array([])
+
+        for i in range(1, counters[0]):
+            img = cv2.imread(f"1/frame{i}.jpg")[:,:,0]
+            img = img.reshape(16950)
+            img_list = np.append(img_list, [img])
